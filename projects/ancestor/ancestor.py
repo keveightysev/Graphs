c859b41eac68a295523697bdb01dc741138fb445
Kevin Smith <keveightysev@gmail.com>
2019-10-20 20:40:02 -0500

    Transferred over Graph and Queue classes

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,39 @@
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+
+class Graph:
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex):
+        self.vertices[vertex] = set()
+
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError('Cannot create edge from vertices')
+
+    def get_parent(self, vertex):
+        if vertex in self.vertices:
+            return self.vertices[vertex]
+
 
 def earliest_ancestor(ancestors, starting_node):
     pass
